e5431826bf311a16734d001478eef81cb3ba4c29
Scott Ferguson <scottwferg@gmail.com>
2009-10-08 21:11:57 -0500

    Get rid of ugly string parsing to find changed files

--- gitHandler.py
+++ gitHandler.py
@@ -2,45 +2,16 @@ import git
 import sys, os
 import getopt
 
-findParentCommit = lambda x: x.data[53:93]
-
 try:
     repo = git.Repo(os.getcwd())
 except git.errors.InvalidGitRepositoryError:
     print 'Error: Not a git repository'
 
-def parseFileList(diff):
-    '''
-    Parses filenames out of a diff
-    '''
-    result = []
-
-    while True:
-        try:
-            index = diff.index('---')
-            # Arbitrary offset of 67 characters to pull out just the filename from the 
-            # string. Add it to the result.
-            result.append(diff[index:index + 67].split(' ')[1].split('a/')[1].split('\n')[0])
-            # Remove the first occurrence of '---' so we can loop back and do it
-            # again
-            diff = diff.replace('---', '+++', 1)
-        except ValueError:
-            break
-
-    return result
-
 def findChangedFiles(commit):
     '''
     Finds all of the affected files within a given commit
     '''
-    try:
-        blob = repo.blob(commit)
-        diff = repo.diff(repo.commit(commit), repo.commit(findParentCommit(blob)))
-    except git.errors.GitCommandError:
-        print 'Error: Not a git repository'
-        return
-
-    return parseFileList(diff)
+    return ['%s' % diff.a_path for diff in repo.commit(commit).diffs]
 
 def getCommitMessage(commit):
     '''
