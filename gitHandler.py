4c9f4ad9dae997d4bb9160e40db69c60b2941632
Scott Ferguson <sferguson@quikorder.com>
2009-10-20 12:57:31 -0500

    Fix merge conflict leftovers

--- gitHandler.py
+++ gitHandler.py
@@ -9,11 +9,31 @@ try:
 except git.errors.InvalidGitRepositoryError:
     print 'Error: Not a git repository'
 
+<<<<<<< HEAD
 def findParentCommit(commit):
     '''
     Returns parents of a commit
     '''
     return repo.commit(commit).parents[0].id
+=======
+def parseFileList(diff):
+    '''
+    Parses filenames out of a diff
+    '''
+    result = []
+
+    while True:
+        try:
+            index = diff.index('---')
+            # Arbitrary offset of 67 characters to pull out just the filename from the 
+            # string. Add it to the result.
+            result.append(diff[index:index + 67].split(' ')[1].split('a/')[1].split('\n')[0])
+            # Remove the first occurrence of '---' so we can loop back and do it
+            # again
+            diff = diff.replace('---', '+++', 1)
+        except ValueError:
+            break
+>>>>>>> e47e1ca... Add command line option to display all commits since a particular tag
 
 def findCommitAuthor(commit): 
     '''
@@ -21,6 +41,7 @@ def findCommitAuthor(commit):
     '''
     return str(repo.commit(commit).author)
 
+<<<<<<< HEAD
 def findCommitCommitter(commit): 
     '''
     Gets committer of a commit
@@ -32,6 +53,18 @@ def findCommitAuthoredDate(commit):
     Gets commit authored date
     '''
     return time.strftime("%a, %d %b %Y %H:%M", repo.commit(commit).authored_date)
+=======
+def findChangedFiles(commit):
+    '''
+    Finds all of the affected files within a given commit
+    '''
+    try:
+        blob = repo.blob(commit)
+        diff = repo.diff(repo.commit(commit), repo.commit(findParentCommit(blob)))
+    except git.errors.GitCommandError:
+        print 'Error: Not a git repository'
+        return
+>>>>>>> e47e1ca... Add command line option to display all commits since a particular tag
 
 def findCommitCommittedDate(commit): 
     '''
@@ -92,6 +125,7 @@ def getCommitsSinceTag(tag):
         print 'Error: Commit not found'
         return False
 
+<<<<<<< HEAD
     return result
 
 def getCurrentRef():
@@ -116,6 +150,20 @@ def cherryPickCommit(hash, noCommit = False):
 def cleanBranch(force = False):
     if repo.is_dirty or force:
         cmd.execute('git reset --hard')
+=======
+def getCommitsSinceTag(tag):
+    '''
+    Returns all commits since the given tag
+    '''
+    try:
+        commitList = repo.commits_between(tag, 'HEAD')
+        result = [commit.id for commit in commitList]
+    except git.errors.GitCommandError:
+        print 'Error: Commit not found'
+        return False
+
+    return result
+>>>>>>> e47e1ca... Add command line option to display all commits since a particular tag
     
 def main():
     options, remainder = getopt.getopt(sys.argv[1:], 'c:', 'commit=');
